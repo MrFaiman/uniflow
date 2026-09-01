ac22b939f2e45611bc61164d58bf0eacda8cb8f6
yarden.nitsan@gmail.com <yarden.nitsan@gmail.com>
2026-09-01 13:57:15 +0300

    Add file monitor

---
+++ python/client/src/client/file_monitor/monitor.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+from client.common.round_robin import RoundRobin
+
+SMALL_FILE_LIMIT = 10 * 1024 * 1024
+
+class FileMonitor:
+    def __init__(self, watch_folder: Path):
+        self.watch_folder = watch_folder
+        self.known_files = {}
+        self.round_robin = RoundRobin(3)
+
+    def get_files(self) -> list[Path]:
+        files = []
+
+        for item in self.watch_folder.iterdir():
+            if item.is_file():
+                files.append(item)
+
+        return files
+
+    def get_changed_files(self) -> list[Path]:
+        changed_files = []
+
+        for file in self.get_files():
+            modified_time = file.stat().st_mtime_ns
+
+            if (
+                file not in self.known_files
+                or self.known_files[file] != modified_time
+            ):
+                changed_files.append(file)
+
+            self.known_files[file] = modified_time
+
+        return changed_files
+
+    def is_small_file(self, file: Path) -> bool:
+        return file.stat().st_size < SMALL_FILE_LIMIT
+
+    def get_sender(self, file: Path) -> int:
+        if self.is_small_file(file):
+            return self.round_robin.next_sender()
+
+        return -1
