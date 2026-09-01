a0f4b5e6d9a3f450f321288434c59781fc356ae3
yairoren1379 <yairoren2312@gmail.com>
2026-09-01 09:54:27 +0300

    fixed the router recive port

--- router.py
+++ router.py
@@ -1,6 +1,7 @@
 import socket
+import select
 import random
-from config import ROUTER_IP, ROUTER_PORT, RX_HOST, RX_PORT_LIST,MAX_UDP_PACKET_SIZE, DisruptionProbabilities
+from config import ROUTER_IP, RX_HOST, RX_PORT_LIST, DisruptionProbabilities
 
 def apply_bit_flip(data):
     if not data:
@@ -12,27 +13,38 @@ def apply_bit_flip(data):
     return bytes(data_bytearray)
 
 def start_router():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((ROUTER_IP, ROUTER_PORT))
+    listening_sockets = {}
     
-    print(f"[Router] Listening on {ROUTER_IP}:{ROUTER_PORT}...")
+    for port in RX_PORT_LIST:
+        recv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        recv_sock.bind((ROUTER_IP, port))
+        listening_sockets[recv_sock] = port
+        print(f"[Router] Listening for TX on {ROUTER_IP}:{port}...")
+        
+    send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
     while True:
-        data, addr = sock.recvfrom(MAX_UDP_PACKET_SIZE)
+        readable, _, _ = select.select(list(listening_sockets.keys()), [], [])
         
-        if random.random() < DisruptionProbabilities.PACKET_LOSS:
-            continue
+        for sock in readable:
+            data, addr = sock.recvfrom(65535)
+            intended_port = listening_sockets[sock]
             
-        if random.random() < DisruptionProbabilities.BIT_FLIP:
-            data = apply_bit_flip(data)
-            
-        target_port = random.choice(RX_PORT_LIST)
-        
-        if random.random() < DisruptionProbabilities.MISROUTING:
-            wrong_ports = [p for p in RX_PORT_LIST if p != target_port]
-            target_port = random.choice(wrong_ports)
+            if random.random() < DisruptionProbabilities.PACKET_LOSS:
+                print(f"[X] Packet Loss: Dropped packet from {addr} to {intended_port}")
+                continue
                 
-        sock.sendto(data, (socket.gethostbyname(RX_HOST), target_port))
+            if random.random() < DisruptionProbabilities.BIT_FLIP:
+                data = apply_bit_flip(data)
+                
+            target_port = intended_port
+            
+            if random.random() < DisruptionProbabilities.MISROUTING:
+                wrong_ports = [p for p in RX_PORT_LIST if p != intended_port]
+                target_port = random.choice(wrong_ports)
+                print(f"[?] Misrouting: Redirecting from {intended_port} to {target_port}")
+                    
+            send_sock.sendto(data, (socket.gethostbyname(RX_HOST), target_port))
 
 if __name__ == "__main__":
     start_router()
